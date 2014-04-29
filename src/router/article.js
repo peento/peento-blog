a83899d2824cf5e5e1e0fd71f43414af9d61a433
Zongmin Lei <leizongmin@gmail.com>
2014-04-29 17:43:52 +0800

    文章列表页面

--- src/router/article.js
+++ src/router/article.js
@@ -13,7 +13,14 @@ module.exports = function (ns, router) {
 
       function (next) {
         app.call('article.get_list', req.query, function (err, list) {
-          res.setLocals('articles', list);
+          res.setLocals('article_list', list);
+          next(err);
+        });
+      },
+
+      function (next) {
+        app.call('article.get_count', req.query, function (err, count) {
+          res.setLocals('article_count', count);
           next(err);
         });
       }
