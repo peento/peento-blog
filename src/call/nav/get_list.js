bd1a21bbe912094a796f53c9665a3e66c1b9264e
Zongmin Lei <leizongmin@gmail.com>
2014-05-16 11:34:11 +0800

    call.nav.get_list

---
+++ src/call/nav/get_list.js
@@ -0,0 +1,25 @@
+/**
+ * Call: nav.get_list
+ *
+ * @author Zongmin Lei <leizongmin@gmail.com>
+ */
+
+module.exports = function (ns, debug) {
+  var utils = require('../../lib/utils');
+  return function (params, callback) {
+    debug('get list');
+    var nav_list = ns('model.nav_list');
+    params = utils.cloneObject(params);
+
+    var query = {};
+    if (params.parent) query.parent = params.parent;
+    if (params.type >= 0) query.type = params.type;
+
+    if (!params.order) params.order = '';
+    params.order = 'sort:desc,' + params.order;
+
+    var options = nav_list.formatListOptions(params);
+    nav_list.list(query, options, callback);
+
+  }
+};
